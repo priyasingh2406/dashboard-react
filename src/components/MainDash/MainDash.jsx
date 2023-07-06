e201c6339fb0992667a4268c565ab10f51d1fe8c
priyasingh2406 <priyabhadoriya2406@gmail.com>
2023-07-06 11:49:17 +0530

    first commit~

---
+++ src/components/MainDash/MainDash.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Cards from '../Cards/Cards'
+import './MainDash.css'
+import Table from '../Table/Table'
+// import { Table } from '../Table';
+// import { Table } from '@mui/material/Table';
+export const MainDash = () => {
+  return (
+    <div className="MainDash">
+        <h1>Dashboard</h1>  
+        <Cards/>
+      <Table/>
+    </div>
+  )
+}
+
+export default MainDash;
+
